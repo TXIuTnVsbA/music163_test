0cdf3575151f888d5c095493319a3ecb135b19f5
TXIuTnVsbA <qqsmallyu80@gmail.com>
2017-09-07 15:42:03 +0800

    Create test_get_song_url.py

---
+++ test_get_song_url.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+import json
+import os
+import base64
+from Crypto.Cipher import AES
+from pprint import pprint
+import binascii
+import hashlib
+from http.cookiejar import LWPCookieJar
+
+modulus = ('00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7'
+           'b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280'
+           '104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932'
+           '575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b'
+           '3ece0462db0a22b8e7')
+nonce = '0CoJUm6Qyw8W8jud'
+pubKey = '010001'
+def aesEncrypt(text, secKey):
+    pad = 16 - len(text) % 16
+    text = text + chr(pad) * pad
+    encryptor = AES.new(secKey, 2, '0102030405060708')
+    ciphertext = encryptor.encrypt(text)
+    ciphertext = base64.b64encode(ciphertext).decode('utf-8')
+    return ciphertext
+
+def rsaEncrypt(text, pubKey, modulus):
+    text = text[::-1]
+    rs = pow(int(binascii.hexlify(text), 16), int(pubKey, 16), int(modulus, 16))
+    return format(rs, 'x').zfill(256)
+
+def createSecretKey(size):
+    return binascii.hexlify(os.urandom(size))[:16]
+
+# 歌曲加密算法, 基于https://github.com/yanunon/NeteaseCloudMusic脚本实现
+def encrypted_id(id):
+    magic = bytearray('3go8&$8*3*3h0k(2)2', 'u8')
+    song_id = bytearray(id, 'u8')
+    magic_len = len(magic)
+    for i, sid in enumerate(song_id):
+        song_id[i] = sid ^ magic[i % magic_len]
+    m = hashlib.md5(song_id)
+    result = m.digest()
+    result = base64.b64encode(result)
+    result = result.replace(b'/', b'_')
+    result = result.replace(b'+', b'-')
+    return result.decode('utf-8')
+
+# 登录加密算法, 基于https://github.com/stkevintan/nw_musicbox脚本实现
+def encrypted_request(text):
+    text = json.dumps(text)
+    secKey = createSecretKey(16)
+    encText = aesEncrypt(aesEncrypt(text, nonce), secKey)
+    encSecKey = rsaEncrypt(secKey, pubKey, modulus)
+    data = {'params': encText, 'encSecKey': encSecKey}
+    return data
+action='http://music.163.com/weapi/song/enhance/player/url?csrf_token='
+header = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip,deflate,sdch',
+            'Accept-Language': 'zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'music.163.com',
+            'Referer': 'http://music.163.com/search/',
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36',
+}
+
+text={
+    "ids":"[414980579]",
+    "br":128000,
+    "csrf_token":""
+}
+data = encrypted_request(text)
+session = requests.Session()
+tmp1=session.post(action,data=data,headers=header,timeout=2)
+tmp1.encoding = 'UTF-8'
+pprint(json.loads(tmp1.text))
